1272cd2bd8e8fad5676b0e43abfe05c4b37827b3
Phil-Chuong <68432036+Phil-Chuong@users.noreply.github.com>
2023-09-19 18:52:31 +0100

    Starting new Reddit App project

---
+++ src/Components/App/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Reddit Me Now
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
