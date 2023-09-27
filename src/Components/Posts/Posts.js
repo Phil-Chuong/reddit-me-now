46e7514b76ec7d2ed10da017c7582f79735c517f
Phil-Chuong <68432036+Phil-Chuong@users.noreply.github.com>
2023-09-27 15:35:04 +0100

    Finally render the data on UI...with CSS

--- src/Components/Posts/Posts.js
+++ src/Components/Posts/Posts.js
@@ -5,8 +5,10 @@ import { useEffect } from "react";
 import { fetchPosts } from "../../API/RedditSlice";
 
 
+
+
 const Posts = () => {    
-    const posts = useSelector((state) => state.redditPosts.posts);
+    const posts = useSelector((state) => state.redditPosts.posts)
     const loading = useSelector((state) => state.redditPosts.loading);
     const error = useSelector((state) => state.redditPosts.error);
     const dispatch = useDispatch();
@@ -28,13 +30,20 @@ const Posts = () => {
     }
 
     return (
-        <div className="card">              
-            <h2>List of Posts</h2>
-                <ul>            
-                    {posts.map((post) => (
-                <li key={post.data}>{post.title}</li>
-                    ))}
-                </ul>
+        <div className="posts-container"> 
+            {posts.map((post) => (
+                <div className="card">
+                <li key={post.id}>
+                    <section className="card-header">
+                        <div className="title-name"><p>{post.title}</p></div>
+                        <div className="subreddit-name"><p>{post.subreddit}</p></div>
+                    </section>                  
+                    
+                    <br/>
+                    <img className='post-image' src={post.url} alt='content'/>
+                </li>
+                </div>
+            ))}
         </div>      
     )
 }
