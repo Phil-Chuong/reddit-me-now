662d3b815facb6c5f5e0fc252dd97ce922948d4b
Phil-Chuong <68432036+Phil-Chuong@users.noreply.github.com>
2023-11-30 12:45:01 +0000

    cypress test removed

--- src/Components/Posts/Posts.cy.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Posts from './Posts'
-
-describe('<Posts />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<Posts />)
-  })
-})
--- src/Components/Subreddit/Subreddit.cy.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Subreddit from './Subreddit'
-
-describe('<Subreddit />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<Subreddit />)
-  })
-})
